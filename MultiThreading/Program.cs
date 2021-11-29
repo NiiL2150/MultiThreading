7d10bc9c3ed26ff49afbd970727d8479564973e7
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2021-11-29 20:37:09 +0400

    All tasks complete with console

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 namespace MultiThreading
@@ -47,51 +50,7 @@ namespace MultiThreading
 
             #endregion
 
-            /*
-            ParameterizedThreadStart start = new ParameterizedThreadStart(ShowNabor);
-            int threadCount = 10;
-            for (int i = 0; i < threadCount; i++)
-            {
-                int[] StartEnd = new int[3];
-                StartEnd[0] = 0;
-                StartEnd[1] = 50;
-                StartEnd[2] = i;
-                Thread thread1 = new Thread(start);
-                thread1.Start(StartEnd);
-            }
-            */
-
-            collect = new int[10000]; 
-            ParameterizedThreadStart start = new ParameterizedThreadStart(RandomTenTh);
-            Thread thread = new Thread(start);
-            thread.Priority = ThreadPriority.AboveNormal;
-            thread.Start(collect);
-
-            int[] results = new int[3];
-
-            Thread.Sleep(100);
-
-            ParameterizedThreadStart start1 = new ParameterizedThreadStart(Max);
-            Thread thread1 = new Thread(start1);
-            thread1.Start(results);
-
-            ParameterizedThreadStart start2 = new ParameterizedThreadStart(Min);
-            Thread thread2 = new Thread(start2);
-            thread2.Start(results);
-
-            ParameterizedThreadStart start3 = new ParameterizedThreadStart(Avg);
-            Thread thread3 = new Thread(start3);
-            thread3.Start(results);
-
-            ParameterizedThreadStart start4 = new ParameterizedThreadStart(ShowConsole);
-            Thread thread4 = new Thread(start4);
-            thread4.Start(collect);
-
-            Thread.Sleep(10000);
-
-            Console.WriteLine($"\nMax = {results[0]}");
-            Console.WriteLine($"Min = {results[1]}");
-            Console.WriteLine($"Avg = {results[2]}");
+            ChooseTask();
         }
 
         #region InClass
@@ -125,12 +84,134 @@ namespace MultiThreading
         }
         #endregion
 
+        private static void ChooseTask()
+        {
+            Console.WriteLine("1 for first task, 2 for second, 3 for loading last values");
+            int z = int.Parse(Console.ReadLine());
+            if (z == 1)
+            {
+                Task1();
+            }
+            else if (z == 2)
+            {
+                Task2();
+            }
+            else if (z == 3)
+            {
+                Task3();
+            }
+        }
+
+        private static void Task1()
+        {
+            ParameterizedThreadStart start = new ParameterizedThreadStart(ShowNabor);
+            Console.WriteLine("Enter the number of threads");
+            int threadCount = int.Parse(Console.ReadLine());
+
+            int[] StartEnd = new int[3];
+
+            Console.WriteLine("Enter starting number");
+            StartEnd[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter ending number");
+            StartEnd[1] = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                StartEnd[2] = i;
+                Thread thread1 = new Thread(start);
+                thread1.Start(StartEnd);
+            }
+        }
+
+        private static void Task2()
+        {
+            collect = new int[10000];
+            ParameterizedThreadStart start = new ParameterizedThreadStart(RandomTenTh);
+            Thread thread = new Thread(start);
+            thread.Priority = ThreadPriority.AboveNormal;
+            thread.Start(collect);
+
+            int[] results = new int[3];
+
+            Thread.Sleep(100);
+
+            ParameterizedThreadStart start1 = new ParameterizedThreadStart(Max);
+            Thread thread1 = new Thread(start1);
+            thread1.Start(results);
+
+            ParameterizedThreadStart start2 = new ParameterizedThreadStart(Min);
+            Thread thread2 = new Thread(start2);
+            thread2.Start(results);
+
+            ParameterizedThreadStart start3 = new ParameterizedThreadStart(Avg);
+            Thread thread3 = new Thread(start3);
+            thread3.Start(results);
+
+            ParameterizedThreadStart start4 = new ParameterizedThreadStart(ShowConsole);
+            Thread thread4 = new Thread(start4);
+            thread4.Start(collect);
+
+            ParameterizedThreadStart start5 = new ParameterizedThreadStart(CopyToFile);
+            Thread thread5 = new Thread(start5);
+            thread5.Start(collect);
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine($"\nMax = {results[0]}");
+            Console.WriteLine($"Min = {results[1]}");
+            Console.WriteLine($"Avg = {results[2]}");
+        }
+
+        private static void Task3()
+        {
+            collect = new int[10000];
+            ParameterizedThreadStart start = new ParameterizedThreadStart(ReadFromFile);
+            Thread thread = new Thread(start);
+            thread.Priority = ThreadPriority.AboveNormal;
+            thread.Start(collect);
+
+            int[] results = new int[3];
+
+            Thread.Sleep(1000);
+
+            ParameterizedThreadStart start1 = new ParameterizedThreadStart(Max);
+            Thread thread1 = new Thread(start1);
+            thread1.Start(results);
+
+            ParameterizedThreadStart start2 = new ParameterizedThreadStart(Min);
+            Thread thread2 = new Thread(start2);
+            thread2.Start(results);
+
+            ParameterizedThreadStart start3 = new ParameterizedThreadStart(Avg);
+            Thread thread3 = new Thread(start3);
+            thread3.Start(results);
+
+            ParameterizedThreadStart start4 = new ParameterizedThreadStart(ShowConsole);
+            Thread thread4 = new Thread(start4);
+            thread4.Start(collect);
+
+            ParameterizedThreadStart start5 = new ParameterizedThreadStart(CopyToFile);
+            Thread thread5 = new Thread(start5);
+            thread5.Start(collect);
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine($"\nMax = {results[0]}");
+            Console.WriteLine($"Min = {results[1]}");
+            Console.WriteLine($"Avg = {results[2]}");
+        }
+
         private static void ShowNabor(object startEnd)
         {
             int[] StartEnd = startEnd as int[];
+
+            //Copy of the variable
+            int thread = StartEnd[2];
+
             for (int i = StartEnd[0]; i < StartEnd[1]; i++)
             {
-                Console.WriteLine($"Thread {StartEnd[2]} - {i}");
+                Console.WriteLine($"Thread {thread} - {i}");
             }
         }
 
@@ -189,6 +270,40 @@ namespace MultiThreading
             foreach (var item in colle)
             {
                 Console.WriteLine(item);
+            }
+        }
+
+        private static void CopyToFile(object coll)
+        {
+            int[] colle = coll as int[];
+            string path = Environment.CurrentDirectory + @"\savefile.txt";
+
+            using (var writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                foreach (var item in colle)
+                {
+                    writer.Write(item);
+                }
+            }
+        }
+
+        private static void ReadFromFile(object coll)
+        {
+            string path = Environment.CurrentDirectory + @"\savefile.txt";
+
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            int[] colle = coll as int[];
+
+            using (var reader = new BinaryReader(File.Open(path, FileMode.Open)))
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    colle[i] = reader.ReadInt32();
+                }
             }
         }
     }
